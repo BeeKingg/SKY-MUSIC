8388047dd641a5b6eba6e5cd71ec73e86e141a85
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-27 20:19:39 +0700

    bug fixes

--- helpers/filters.py
+++ helpers/filters.py
@@ -1,11 +1,15 @@
-from typing import Union, List
+# Copyright (C) 2021 By VeezProject
+
+from typing import List, Union
 
 from pyrogram import filters
 
 from config import COMMAND_PREFIXES
 
-other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
-other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+other_filters2 = (
+    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+)
 
 
 def command(commands: Union[str, List[str]]):
