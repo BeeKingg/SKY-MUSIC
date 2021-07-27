5c76c58e0eecf71b3916a5770f9d24c292f7c4ec
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-27 17:31:07 +0700

    fixing gcast
    now gcast function can be used

--- handlers/gcast.py
+++ handlers/gcast.py
@@ -1,25 +1,35 @@
-from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant
+# Copyriht (C) 2021 By VeezProject
+
 import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.types import Dialog, Chat, Message
+from pyrogram.errors import UserAlreadyParticipant
+
+from callsmusic.callsmusic import client as veez
 from config import SUDO_USERS
 
 @Client.on_message(filters.command(["gcast"]))
-async def bye(client, message):
+async def broadcast(_, message: Message):
     sent=0
     failed=0
-    if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("Starting Gcast")
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`memulai global cast...`")
         if not message.reply_to_message:
-            await lol.edit("Reply to any text message to gcast sir")
+            await wtf.edit("tolong balas ke pesan apapun untuk broadcast!")
             return
-        msg = message.reply_to_message.text
-        async for dialog in client.iter_dialogs():
+        lmao = message.reply_to_message.text
+        async for dialog in veez.iter_dialogs():
             try:
-                await client.send_message(dialog.chat.id, msg)
+                await veez.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await wtf.edit(f"`global cast...` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
+                await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
-            await asyncio.sleep(3)
-        await message.reply_text(f"Gcasted message to {sent} chats. Failed {failed} chats.")
+                #await wtf.edit(f"`Melakukan gcast...` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
+                
+            
+        await message.reply_text(f"`gcast berhasil ` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
