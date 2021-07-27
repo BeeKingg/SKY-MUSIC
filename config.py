aca32fcf0702fcd4e9a53d83fbda1c2b6e13708d
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-27 23:40:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,25 @@
+import os
 from os import getenv
 
 from dotenv import load_dotenv
 
-load_dotenv()
+if os.path.exists("local.env"):
+  load.dotenv("local.env")
 
+que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME", "Veez Music Bot")
 BOT_USERNAME = getenv("BOT_USERNAME", "veezmusicbot")
-
+admins = {}
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/d91623e9cefcef8fb3aa8.png")
 PROJECT_NAME = getenv("PROJECT_NAME", "Veez Music Project")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
 GROUP_SUPPORT = getenv("GROUP_SUPPORT", "gcsupportbots")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "veezasisstant")
 OWNER = getenv("OWNER", "@dlwrml")
-
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
